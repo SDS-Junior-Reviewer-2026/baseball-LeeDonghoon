32f2ee4bd03b566ff73921d10c5b6ab467030422
dhoon <ydh82066@gmail.com>
2026-04-03 13:32:25 +0900

    [feature] 46p

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,4 +2,19 @@ package baseball;
 
 public class Game {
 
+    public void guess(String guessNumber) {
+        if (guessNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (guessNumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        for (char number : guessNumber.toCharArray()) {
+            if (number < '0' || number > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
